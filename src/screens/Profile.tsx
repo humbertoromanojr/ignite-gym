b1b7dcbffe80fda40e697cc99d8a39e03bb41f80
JR DevPM <astronomi@gmail.com>
2022-11-13 12:13:33 -0300

    Implement ScreenHeader, UserPhotos and Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from 'native-base'
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { VStack, Center, ScrollView, Text, Skeleton } from 'native-base'
+
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text>Profile screen</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+      <Center mt={5} px={10}>
+        {
+          photoIsLoading ?
+            <Skeleton 
+              w={PHOTO_SIZE} 
+              h={PHOTO_SIZE} 
+              rounded="full" 
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+            :
+            <UserPhoto 
+              source={{ uri: 'https://github.com/humbertoromanojr.png' }}
+              alt="Imagem do usuário"
+              size={PHOTO_SIZE} 
+              mr={4}
+            />
+        }
+
+        <TouchableOpacity>
+          <Text color="gray.200" fontWeight="bold" fontSize="md" mb={8} mt={2}>
+            Alterar foto
+          </Text>
+        </TouchableOpacity>
+      </Center>
+      </ScrollView>
+    </VStack>
   )
 }
