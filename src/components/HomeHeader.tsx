8aef9d23bac5b55ff97b5340d0674063f0cd4612
JR DevPM <astronomi@gmail.com>
2022-11-13 02:41:20 -0300

    Created HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,11 @@
+import { HStack, Heading, Text } from 'native-base'
+
+export function HomeHeader() {
+  return (
+    <HStack flex={1}>
+      <Text>Olá</Text>
+
+      <Heading>Humberto Jr</Heading>
+    </HStack>
+  )
+}
