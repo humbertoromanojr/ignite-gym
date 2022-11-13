a1397fe070804d9fbd481409a3837948478dafa7
JR DevPM <astronomi@gmail.com>
2022-11-13 11:34:21 -0300

    Integrating HistoryCard with SectionList

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,50 @@
-import { VStack } from 'native-base'
+import { useState } from 'react'
+import { VStack, Heading, SectionList, Text } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.10.22',
+      data: ['Puxada frontal', 'Remada unilateral', 'Bíceps'],
+    },
+    {
+      title: '27.10.22',
+      data: ['Costas', 'Tríceps'],
+    },
+    {
+      title: '28.10.22',
+      data: ['Pernas', 'Abdomen'],
+    }
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico de Exercícios' />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={4}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text mt={5} color="gray.100" fontSize="2xl" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+      />
+
     </VStack>
   )
 }
