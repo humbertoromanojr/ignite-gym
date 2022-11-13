d3b9aa3a0aa8740610d15bafbb43c9bf1fc43d4b
JR DevPM <astronomi@gmail.com>
2022-11-13 05:27:43 -0300

    Finished Group component

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,11 +1,37 @@
-import { Text } from 'native-base'
+import { Text, Pressable, IPressableProps } from 'native-base'
 
-type Props = {
+type Props = IPressableProps & {
   name: string;
+  isActive: boolean;
 }
 
-export function Group({ name }: Props) {
+export function Group({ name, isActive, ...rest }: Props) {
   return (
-    <Text>{name}</Text>
+    <Pressable
+      alignItems="center"
+      justifyContent="center"
+      overflow="hidden"
+      bg="gray.600"
+      rounded="md"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1
+      }}
+      mr={3}
+      w={24}
+      h={10}
+
+      {...rest}
+    >
+      <Text
+        color={isActive ? "green.500" : "gray.200"}
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,33 @@
-import { VStack } from 'native-base'
+import { useState } from 'react'
+import { HStack, VStack, FlatList } from 'native-base'
 
 import { HomeHeader } from '@components/HomeHeader'
+import { Group } from '@components/Group'
 
 export function Home() {
+  const [group, setGroup] = useState(['costas', 'bíceps', 'tríceps', 'ombro', 'pernas'])
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList 
+        data={group}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item} 
+            isActive={groupSelected === item} 
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        maxH={10}
+        my={8}
+      />
     </VStack>
   )
 }
