52aec3c051fb36be91b625a31002c69b3826cafd
JR DevPM <astronomi@gmail.com>
2022-11-13 06:20:01 -0300

    Finished ExerciseCard

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,9 +1,36 @@
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Heading, Image, HStack, VStack, Text, Icon } from 'native-base'
+import { Entypo } from '@expo/vector-icons'
 
-export function ExerciseCard() {
+type Props = TouchableOpacityProps & {
+
+}
+
+export function ExerciseCard({...rest}: Props) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity {...rest}>
+      <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+        <Image 
+          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH3nymzg_sWcIlh58heTh2cYoa2KKashsuA5YKP9QPHmni-1sRfSXXtd7E17FUs8iNZLY&usqp=CAU' }}
+          alt="Ilustração de como se deve malhar remada unilateral"
+          rounded="md"
+          resizeMode="center"
+          mr={4}
+          w={16}
+          h={16}
+        />
 
+        <VStack flex={1}>
+          <Heading fontSize="lg" color="white">
+            Remada unilateral
+          </Heading>
+          <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
+            3 séries X 12 repetições 
+          </Text>
+        </VStack>
+
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
     </TouchableOpacity>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import { HStack, VStack, FlatList } from 'native-base'
+import React, { useState } from 'react'
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base'
 
 import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 
 export function Home() {
   const [group, setGroup] = useState(['costas', 'bíceps', 'tríceps', 'ombro', 'pernas'])
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra'])
   const [groupSelected, setGroupSelected] = useState('costas')
 
   return (
@@ -28,6 +30,28 @@ export function Home() {
         maxH={10}
         my={8}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Ecercícios
+          </Heading>
+
+          <Text  color="gray.200" fontSize="md">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   )
 }
