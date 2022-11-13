06179946694e2ceb1dfe53dbcb0caa215d587f8e
JR DevPM <astronomi@gmail.com>
2022-11-13 15:23:10 -0300

    Finished header exercise screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,40 @@
-import { Center, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { VStack, HStack, Heading, Icon, Text } from 'native-base'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+  
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise screen</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+
+        <HStack justifyContent="space-between" alignItems="center" mt={4} mb={4}>
+          <Heading color="gray.100" fontSize="lg" flexShrink={1} numberOfLines={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
