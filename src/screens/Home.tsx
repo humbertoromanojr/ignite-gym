fd958be30b5ef3af54593b895c029ecfccb22411
JR DevPM <astronomi@gmail.com>
2022-11-13 06:28:56 -0300

    Avoid caseSensitive error, less code

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -20,7 +20,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group 
             name={item} 
-            isActive={String(groupSelected).toLocaleUpperCase() === String(item).toLocaleUpperCase()} 
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()} 
             onPress={() => setGroupSelected(item)}
           />
         )}
