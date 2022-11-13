098d79ffcefc32f98f1625ffa420823ce656878a
JR DevPM <astronomi@gmail.com>
2022-11-13 03:20:02 -0300

    Configuring UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, VStack,  Heading, Text } from 'native-base'
+import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" alignItems="center" pt={16} pb={5} px={8}>
+      <UserPhoto 
+        source={{ uri: 'https://github.com/humbertoromanojr.png' }}
+        size={16} 
+        mr={4}
+        alt="Imagem do usuário"
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá
@@ -11,6 +18,10 @@ export function HomeHeader() {
         <Heading color="gray.100" fontSize="md">
           Humberto Jr
         </Heading>
+
+        <Text color="gray.100" fontSize="sm">
+          Bem vindo(a) de volta
+        </Text>
       </VStack>
     </HStack>
   )
