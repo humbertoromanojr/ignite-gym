596c72ba54ccd7c4e60dc347b3753efd678a5cff
JR DevPM <astronomi@gmail.com>
2022-11-13 10:57:47 -0300

    Finished HistoryCard component

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,26 @@
+import { HStack, VStack, Heading, Text } from 'native-base'
+
+export function HistoryCard() {
+  return (
+    <HStack 
+      bg="gray.600" w="full" 
+      alignContent="center"
+      rounded="md"
+      px={5} py={4} mb={3}
+    >
+      <VStack flex={1} mr={5}>
+        <Heading fontSize="md" color="white" textTransform="capitalize">
+          Costas
+        </Heading>
+
+        <Text color="gray.200" fontSize="lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="gray.300" fontSize="sm">
+        10:41
+      </Text>
+    </HStack>
+  )
+}
