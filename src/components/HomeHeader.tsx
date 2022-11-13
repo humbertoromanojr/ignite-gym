8f9d976f02257ca239f6abe28adcde9e957a3b63
JR DevPM <astronomi@gmail.com>
2022-11-13 03:32:14 -0300

    Finished HomeHeader with icon logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,7 @@
-import { HStack, VStack,  Heading, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { HStack, VStack,  Heading, Text, Icon } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
@@ -6,11 +9,12 @@ export function HomeHeader() {
     <HStack bg="gray.600" alignItems="center" pt={16} pb={5} px={8}>
       <UserPhoto 
         source={{ uri: 'https://github.com/humbertoromanojr.png' }}
+        alt="Imagem do usuário"
         size={16} 
         mr={4}
-        alt="Imagem do usuário"
       />
-      <VStack>
+
+      <VStack flex={1}> 
         <Text color="gray.100" fontSize="md">
           Olá
         </Text>
@@ -23,6 +27,15 @@ export function HomeHeader() {
           Bem vindo(a) de volta
         </Text>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.100"
+          size={8}
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
