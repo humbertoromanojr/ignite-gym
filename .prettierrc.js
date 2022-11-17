6685905ac7ce96916695c6d11cce2b2b462019ec
JR DevPM <astronomi@gmail.com>
2022-11-17 10:51:52 -0300

    Updating prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: false,
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+};
