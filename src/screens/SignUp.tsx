5fc00eb5ff7ef26b7fa77a967b2ac376ebfd3dc4
JR DevPM <astronomi@gmail.com>
2022-11-17 11:02:10 -0300

    Implementing react-hook-form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,36 +1,40 @@
-import { useNavigation } from '@react-navigation/native'
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import {useNavigation} from '@react-navigation/native';
+import {useForm, Controller} from 'react-hook-form';
+import {VStack, Image, Text, Center, Heading, ScrollView} from 'native-base';
 
-import { Input } from '@components/Input'
-import { Button } from '@components/Button'
+import {Input} from '@components/Input';
+import {Button} from '@components/Button';
 
-import LogoSvg from '@assets/logo.svg'
-import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
 
 export function SignUp() {
-  
-  const navigation = useNavigation()
+  const { control, handleSubmit } = useForm();
 
+  const navigation = useNavigation();
   function handleGoBack() {
-    navigation.goBack()
+    navigation.goBack();
+  }
+
+  function handleSignUp(data: any) {
+    console.log(data);
   }
 
   return (
-    <ScrollView 
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
-    >
+    <ScrollView
+      contentContainerStyle={{flexGrow: 1}}
+      showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} pb={10}>
-        <Image 
-          source={BackgroundImg} 
+        <Image
+          source={BackgroundImg}
           defaultSource={BackgroundImg}
-          resizeMode='contain' 
+          resizeMode="contain"
           position="absolute"
-          alt="duas pessoas treinando na academia bicicleta" 
+          alt="duas pessoas treinando na academia bicicleta"
         />
 
         <Center my={20}>
-          <LogoSvg/>
+          <LogoSvg />
 
           <Text color="gray.100" fontSize="sm">
             Treine sua mente e o seu corpo
@@ -42,33 +46,77 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input 
-            placeholder='Nome' 
-            keyboardType="default"
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-          <Input 
-            placeholder='Email' 
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-          <Input 
-            placeholder='Senha' 
-            secureTextEntry
+          <Controller
+            control={control}
+            name="name"
+            render={({field: { onChange, value }}) => (
+              <Input
+                placeholder="Nome"
+                keyboardType="default"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="email"
+            render={({field: { onChange, value }}) => (
+              <Input
+                placeholder="Email"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({field: { onChange, value }}) => (
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry 
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />          
+
+          <Controller
+            control={control}
+            name="confirm_password"
+            render={({field: { onChange, value }}) => (
+              <Input 
+                placeholder="Confirme a senha" 
+                secureTextEntry 
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+              />
+            )}
+          />  
+
+          <Button 
+            title="Criar e acessar" 
+            onPress={handleSubmit(handleSignUp)} 
+          />
         </Center>
 
-        <Button 
+        <Button
           mt={24}
-          title="Voltar para o login" 
-          variant="outline" 
-          onPress={handleGoBack} 
+          title="Voltar para o login"
+          variant="outline"
+          onPress={handleGoBack}
         />
       </VStack>
     </ScrollView>
-  )
+  );
 }
